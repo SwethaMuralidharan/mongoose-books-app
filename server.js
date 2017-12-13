66413db9ab35ca4f87f021a7102478ac2c76b0be
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2017-12-13 15:38:03 -0800

    sprint2 completed

--- server.js
+++ server.js
@@ -20,30 +20,6 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-////////////////////
-//  DATA
-///////////////////
-
-
-
-
-var newBookUUID = 18;
-
-
-
-
-
-
-
-////////////////////
-//  ROUTES
-///////////////////
-
-
-
-
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
@@ -52,7 +28,7 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err,books){
+  db.Book.find().populate('author').exec(function(err,books){
     if(err){
       console.log("index error: "+err);
     }
@@ -63,13 +39,7 @@ app.get('/api/books', function (req, res) {
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
-  // // find one book by its id
-  // console.log('books show', req.params);
-  // for(var i=0; i < books.length; i++) {
-  //   if (books[i]._id === req.params.id) {
-  //     res.json(books[i]);
-  //     break; // we found the right book, we can stop searching
-  //   }
+
  var book_id=req.params.id;
  db.Book.findOne({_id:book_id},function(err,selectedbook)
   {
@@ -80,37 +50,37 @@ app.get('/api/books/:id', function (req, res) {
 
 // create new book
 app.post('/api/books', function (req, res) {
-  // create new book with form data (`req.body`)
-  // console.log('books create', req.body);
-  // var newBook = req.body;
-  // newBook._id = newBookUUID++;
-  // books.push(newBook);
-  // res.json(newBook);
-  db.Book.create(req.body,function(err,data){
-    res.json(data);
-  })
+      // create new book with form data (`req.body`)
+      var newBook = new db.Book({
+        title: req.body.title,
+        image: req.body.image,
+        releaseDate: req.body.releaseDate,
+      });
 
-});
+      // this code will only add an author to a book if the author already exists
+      db.Author.findOne({name: req.body.author}, function(err, author){
+        newBook.author = author;
+        // add newBook to database
+        newBook.save(function(err, book){
+          if (err) {
+            return console.log("create error: " + err);
+          }
+          console.log("created ", book.title);
+          res.json(book);
+        });
+      });
+
+    });
+
 
 // update book
 app.put('/api/books/:id', function(req,res){
-// // get book id from url params (`req.params`)
-//   console.log('books update', req.params);
-//   var bookId = req.params.id;
-//   // find the index of the book we want to remove
-//   var updateBookIndex = books.findIndex(function(element, index) {
-//     return (element._id === parseInt(req.params.id)); //params are strings
-//   });
-//   console.log('updating book with index', deleteBookIndex);
-//   var bookToUpdate = books[deleteBookIndex];
-//   books.splice(updateBookIndex, 1, req.params);
-//   res.json(req.params);
 
  var book_id=req.params.id;
  db.Book.findOne({ _id: book_id }, function(err, foundbook) {
 
         foundbook.title = (req.body.title) ? req.body.title : foundbook.title ;
-        foundbook.author = (req.body.author) ? req.body.author: foundbook.author;
+        foundbook.author.name = (req.body.author) ? req.body.author.name: foundbook.author.name;
         foundbook.image = (req.body.image)? req.body.image :  foundbook.image ;
         foundbook.release_date = (req.body.release_date) ? req.body.release_date : foundbook.release_date ;
 
@@ -122,26 +92,12 @@ app.put('/api/books/:id', function(req,res){
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
-  // // get book id from url params (`req.params`)
-  // console.log('books delete', req.params);
-  // var bookId = req.params.id;
-  // // find the index of the book we want to remove
-  // var deleteBookIndex = books.findIndex(function(element, index) {
-  //   return (element._id === parseInt(req.params.id)); //params are strings
-  // });
-  // console.log('deleting book with index', deleteBookIndex);
-  // var bookToDelete = books[deleteBookIndex];
-  // books.splice(deleteBookIndex, 1);
-  // res.json(bookToDelete);
+
   var book_id=req.params.id;
   db.Book.findOneAndRemove({_id:book_id},function(err,deleted_item){
     res.json(deleted_item);
   })
 });
-
-
-
-
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Book app listening at http://localhost:3000/');
